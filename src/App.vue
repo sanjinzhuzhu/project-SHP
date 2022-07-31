e3fad15dcef8ca47304c105669e69aa247b27b90
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-07-31 16:00:51 +0800

    Setting the routing meta information

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <Header></Header>
     <!-- 路由组件出口的地方 -->
     <router-view></router-view>
-    <Footer></Footer>
+    <!-- <Footer v-show="$route.path =='/home'|| $route.path=='/search'"></Footer> 可以用路由元信息-->
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
