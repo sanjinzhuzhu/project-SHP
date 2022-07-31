9a8c28690267e74a3de44b9cd204e850fa47ac54
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-07-31 22:06:01 +0800

    Route passing parameters

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -1,13 +1,16 @@
 <template>
- <div>我是搜索页</div>   
+  <div>
+    <h1>params参数{{ $route.params.keyword }}=={{keyword}}</h1>
+    <h1>query参数{{ $route.query.k }}==={{k}}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+    name:'',
+    props:['keyword','k']
+};
 </script>
 
 <style>
-
 </style>
