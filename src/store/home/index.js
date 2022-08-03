5284ff3c4b195db39f3161869bff88e43812e8b3
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-08-03 22:08:20 +0800

    Categorical data presentatio- III

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,25 @@
+//从api文件封装的三级联动接口引入(这里接口是老师直接提供的，不知道怎么来的)
+import { reqCategoryList } from "@/api";
+
 //home模块的小仓库
-const state = {};
-const mutations = {};
-const actions = {};
+const state = {
+    categoryList:[],//服务器返回的数据是一个数组，【根据借口返回值初始化的】
+}
+
+const mutations = {
+    CATEGORYLIST(state,categoryList){
+        state.categoryList = categoryList;
+    }
+};
+const actions = {
+    //通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async categoryList({commit}){
+        let result = await reqCategoryList();
+         if(result.code ==200){
+            commit("CATEGORYLIST",result.data);
+         }
+    }
+};
 const getters = {};
 export default {
      state,
