cf8889ab6c52dddaf6bea6993aee9f1b53b68f5e
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-08-15 01:11:16 +0800

    spuSaleAttrValue display

--- src/store/detail/detail.js
+++ src/store/detail/detail.js
@@ -1,9 +1,9 @@
 import { reqGoodsInfo } from "@/api"
 const state = {
-    goodInfo:{}
+    goodInfo: {},
 }
 const mutations = {
-    GETGOODINFO(state,goodInfo){
+    GETGOODINFO(state, goodInfo) {
         state.goodInfo = goodInfo;
     }
 }
@@ -15,7 +15,23 @@ const actions = {
         }
     }
 }
-const getters = {}
+//getters 来简化数据
+const getters = {
+    //路径导航简化的数据
+    categoryView(state) {
+        //因为state.goodInfo初始状态为空对象，所以下面的categoryView属性值为undefined
+        //所以categoryView属性值至少是一个空对象，才不会报错
+        return state.goodInfo.categoryView || {};
+    },
+    //简化产品信息的数据
+    skuInfo(state) {
+        return state.goodInfo.skuInfo || {};
+    },
+    //产品售卖属性的简化
+    spuSaleAttrList(state) {
+        return state.goodInfo.spuSaleAttrList|| [];
+    }
+}
 
 export default {
     state,
